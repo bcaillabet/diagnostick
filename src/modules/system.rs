447cb7092dce4621854dc1cc3c43c8e56ff1f414
kmi-255 <159352635+kmi-255@users.noreply.github.com>
2024-03-07 18:01:45 +0100

    Update system.rs : added sysclock fn
    ⌚

--- src/modules/system.rs
+++ src/modules/system.rs
@@ -3,5 +3,5 @@ pub mod linux;
 use self::linux::Linux;
 
 pub fn detect_linux() -> Linux {
-    Linux::new().parse_os_release().parse_uname().users()
+    Linux::new().parse_os_release().parse_uname().users().sysclock()
 }
